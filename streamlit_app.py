af0a8e571b1e1a0ab60b0d9bce56a051ce4c96e9
Sagnik Ghosh <sagnikghosh44@gmail.com>
2022-08-21 16:54:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
-Streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Hard-Boiled Free-Range Egg')
